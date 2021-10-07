70a5df80d636ff2eb45ed3b56d5fc1d366508949
satakshi <satakshi.pal.civ19@itbhu.ac.in>
2021-10-07 09:57:45 +0530

    added blocks {}

--- C++/Language/Algorithm/Sorting/BubbleSort.cpp
+++ C++/Language/Algorithm/Sorting/BubbleSort.cpp
@@ -14,26 +14,31 @@ void swap(int *xp, int *yp)
 void bubbleSort(int arr[], int n)
 {
 	for (int i = 0; i < n - 1; i++)
-
-		// Last i elements are already in place
+	{
 		for (int j = 0; j < n - i - 1; j++)
+		{
 			if (arr[j] > arr[j + 1])
+			{
 				swap(&arr[j], &arr[j + 1]);
+			}
+		}
+	}
 }
 
 /*Function to print an array */
 void printArray(int arr[], int size)
 {
-	int i;
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
+	{
 		cout << arr[i] << " ";
+	}
 	cout << endl;
 }
 
 // Driver code
 int main()
 {
-	int arr[] = { 6, 334, 25, 1, 22, 11, 90 };
+	int arr[] = {6, 334, 25, 1, 22, 11, 90};
 	int n = sizeof(arr) / sizeof(arr[0]);
 	bubbleSort(arr, n);
 	cout << "Sorted array: \n";
