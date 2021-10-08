c061e02e4f069d6557c4688db7a8d9ee13d96ff7
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2021-10-08 10:20:03 +0530

    update

--- C++/Algorithm/Searching Directory/RecursiveBinarySearch.cpp
+++ C++/Algorithm/Searching Directory/RecursiveBinarySearch.cpp
@@ -14,11 +14,15 @@ if (r >= l){
 		int mid = l + (r - l)/2;
 
 		// If the element is present at the middle itself
-		if (arr[mid] == x) return mid;
+		if (arr[mid] == x){
+             return mid;
+        }
 
 		// If element is smaller than mid, then it can only be present
 		// in left subarray
-		if (arr[mid] > x) return binarySearch(arr, l, mid-1, x);
+		if (arr[mid] > x) {
+            return binarySearch(arr, l, mid-1, x);
+        }
 
 		// Else the element can only be present in right subarray
 		return binarySearch(arr, mid+1, r, x);
