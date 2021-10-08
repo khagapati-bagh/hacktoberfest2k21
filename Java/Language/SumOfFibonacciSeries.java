e045ddf97690f388360b77ae6ea91d91302bc552
RaviTeja_Dev <39962669+Ravitejavelamuri@users.noreply.github.com>
2021-10-08 06:24:53 +0530

    Update SumOfFibonacciSeries.java

--- Java/Language/SumOfFibonacciSeries.java
+++ Java/Language/SumOfFibonacciSeries.java
@@ -3,43 +3,41 @@
         Description : This code finds even sum of fibonacci series till number n
 */
 import java.io.*;
-  
+
 class SumOfFibonacciSeries {
-  
-    // Computing the value of first fibonacci series
-    // and storing the sum of even indexed numbers
-    static int Fib_Even_Sum(int N)
-    {
-        if (N <= 0)
-            return 0;
-  
-        int fib[] = new int[2 * N + 1];
-        fib[0] = 0;
-        fib[1] = 1;
-  
-        // Initializing the sum
-        int s = 0;
-  
-        // Adding remaining numbers
-        for (int j = 2; j <= 2 * N; j++) {
-            fib[j] = fib[j - 1] + fib[j - 2];
-  
-            // Only considering even indexes
-            if (j % 2 == 0)
-                s += fib[j];
-        }
-  
-        return s;
+
+  // Computing the value of first fibonacci series
+  // and storing the sum of even indexed numbers
+  static int Fib_Even_Sum(int N) {
+    if (N <= 0)
+      return 0;
+
+    int fib[] = new int[2 * N + 1];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    // Initializing the sum
+    int s = 0;
+
+    // Adding remaining numbers
+    for (int j = 2; j <= 2 * N; j++) {
+      fib[j] = fib[j - 1] + fib[j - 2];
+
+      // Only considering even indexes
+      if (j % 2 == 0)
+        s += fib[j];
     }
-  
-    // The Driver code
-    public static void main(String[] args)
-    {
-        int N = 11;
-  
-        // Prints the sum of even-indexed numbers
-        System.out.println(
-            "Even sum of fibonacci series till number " + N
-            + " is: " + +Fib_Even_Sum(N));
-    }
+
+    return s;
+  }
+
+  // The Driver code
+  public static void main(String[] args) {
+    int N = 11;
+
+    // Prints the sum of even-indexed numbers
+    System.out.println(
+      "Even sum of fibonacci series till number " + N +
+      " is: " + +Fib_Even_Sum(N));
+  }
 }
