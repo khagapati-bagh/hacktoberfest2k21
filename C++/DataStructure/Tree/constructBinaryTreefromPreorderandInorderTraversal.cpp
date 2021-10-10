4358763bc3108ba57997622c18d37dfd583d9b66
YashGosaliya <58128160+YashGosaliya@users.noreply.github.com>
2021-10-10 22:07:09 +0530

    Update and rename ConstructBinaryTreefromPreorderandInorderTraversal.cpp to constructBinaryTreefromPreorderandInorderTraversal.cpp

--- C++/DataStructure/Tree/constructBinaryTreefromPreorderandInorderTraversal.cpp
+++ C++/DataStructure/Tree/constructBinaryTreefromPreorderandInorderTraversal.cpp
@@ -1,7 +1,21 @@
-
 //Problem link:
 //https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
+//Time Complexity : O(N)
+//Space Complexity : O(N)
+
+
+//Definition for a binary tree node.
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
 class Solution {
 public:
     int preIndex=0;
